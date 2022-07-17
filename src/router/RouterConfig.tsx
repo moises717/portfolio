f042e91862d157fbe4fe366c6b679e68a22ec920
Moises Barillas <moisesrocha717@gmail.com>
2022-07-17 09:54:40 -0600

    new routes

--- src/router/RouterConfig.tsx
+++ src/router/RouterConfig.tsx
@@ -1,8 +1,10 @@
 import { RouteObject } from 'react-router-dom';
 
+import Layout from '../layout';
 import { Home } from '../pages/Home/Home';
 import { Skill } from '../pages/Skills/Skill'
-import Layout from '../layout';
+import { Projects } from '../pages/Projects/Projects';
+import { Contact } from '../pages/Contact/Contact';
 
 export const RouterConfig: RouteObject[] = [
 	{
@@ -17,6 +19,15 @@ export const RouterConfig: RouteObject[] = [
 			{
 				path: '/skills',
 				element: <Skill />,
+			},
+			{
+				path: '/projects',
+				element: <Projects />,
+
+			},
+			{
+				path: '/contact',
+				element: <Contact />,
 			}
 		],
 	},
