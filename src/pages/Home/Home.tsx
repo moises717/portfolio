82f9d1fa692769ebc3489433948c5014e49b20d4
Moises Barillas <moisesrocha717@gmail.com>
2022-07-21 21:19:27 -0600

    fix text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,10 @@
 import { ContentTitle } from '@components/ContentTitle/ContentTitle';
 import { GitHub, Linkedin, Book, User } from '@icons/index';
-import { getYears } from '@utils/Age';
+import { getAge } from '@utils/Age';
 
 import './Home.scss'
 
-const age = getYears('1998-9-20');
-const learningDate = getYears('2019-1-1');
+const age = getAge();
 
 export const Home = () => {
     return (
@@ -29,11 +28,11 @@ export const Home = () => {
                 <div className="home__about-me">
                     <ContentTitle title='Sobre mi' textIcon={<Book />} />
                     <p>
-                        Tengo {age} años, actualmente me encuentro cursando la carrera de ingeniería en sistemas. soy una persona al que le gustan los retos y aprender cosas nuevas cada dia, lo cual me ah llevado a conocer entornos de desarrollo como <strong>frontend</strong> y <strong>backend</strong>.
+                        Tengo {age} años, actualmente me encuentro cursando la carrera de ingeniería en sistemas, me gustan los retos y aprender cosas nuevas cada dia, lo cual me ah llevado a conocer entornos de desarrollo como <strong>frontend</strong> y <strong>backend</strong>.
                     </p>
                     <ContentTitle title='Mi historia' textIcon={<User />} />
                     <p>
-                        Hace aproximadamente {learningDate} años hice una búsqueda en internet, <b>¿Como se hace una pagina web?</b> y uno de los resultados fue <b>html, css y javascript</b>. <strong>javascript</strong> es un lenguaje de programación al que le he dedicado tiempo, desde como funciona en el navegador, hasta como se ejecuta en el servidor. Fue una pregunta sencilla pero que desencadeno mi curiosidad y me insto a aprender nuevas tecnologías.
+                        Hace 3 años hice una búsqueda en internet, <b>¿Como se hace una pagina web?</b> y uno de los resultados fue <b>html, css y javascript </b>, <strong>javascript</strong> es un lenguaje de programación al que le he dedicado tiempo, desde como funciona en el navegador, hasta como se ejecuta en el servidor. Fue una pregunta sencilla pero que desencadeno mi curiosidad y me insto a aprender nuevas tecnologías.
                     </p>
                 </div>
             </div>
