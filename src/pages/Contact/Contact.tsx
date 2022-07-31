bf721ee659d5fbe7ef3459f3ab2e42aed0fd2318
Moises Barillas <moisesrocha717@gmail.com>
2022-07-30 22:07:06 -0600

    working on contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,21 @@
+import './contact.scss'
+
 export const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='contact'>
+            <form action='mailto:moisesrocha717@gmail.com' method='POST'>
+                <div className="contact__form">
+                    <div className='contact__form--name'>
+                        <label htmlFor="name">Nombre</label>
+                        <input className='contact__form--name' type="text" name='name' autoFocus placeholder='Nombre completo' />
+                    </div>
+                    <div className='contact__form--message'>
+                        <label htmlFor="message">Mensaje</label>
+                        <textarea name="message" />
+                    </div>
+                    <button type='submit'>Enviar</button>
+                </div>
+            </form>
+        </div>
     )
 }
