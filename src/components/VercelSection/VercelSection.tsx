83c2b72b33fbccc3bc56267043b75bcce9400e8b
Moises Barillas <moisesrocha717@gmail.com>
2022-08-16 16:24:07 -0600

    hook for get vercel info

--- src/components/VercelSection/VercelSection.tsx
+++ src/components/VercelSection/VercelSection.tsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from "react"
-
-import { getVercelDeployment } from "@services/vercel"
 import { Project } from "@interfaces/VercelDeployment";
+import { useVercelDeployments } from "@hooks/useVercelDeployments";
 
 import { DeploymentItem } from "../DeploymentItem/DeploymentItem";
 import { ProjectSection } from "../ProjectSection/ProjectSection";
 import { List } from "../Skeleton/List/List";
 
 export const VercelSection = () => {
-    const [projects, setProjects] = useState<Project[]>();
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-        setIsLoading(true);
-        getVercelDeployment().then(({ projects }) => {
-            setProjects(projects);
-            setIsLoading(false);
-        })
-    }, [])
-
+    const { isLoading, projects } = useVercelDeployments();
     return (
         <ProjectSection title={`Despliegues (${projects?.length || 0}) 🚀`} isLoading={isLoading} loadingComponent={<List lines={4} quantity={4} numberOfItems={4} showItems title />}>
             <>
