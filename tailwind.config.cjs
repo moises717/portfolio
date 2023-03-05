d4969e321548eeeffa19cae111318430f43b5cb4
Moises Barillas <moisesrocha717@gmail.com>
2023-03-05 09:24:45 -0600

    add navigation items

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#191d2b',
+					100: '#1e1568',
+				},
+				secondary: {
+					DEFAULT: '#27ae60',
+					100: '#6e5ef8',
+				},
+				tertiary: {
+					DEFAULT: '#f56692',
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+	darkMode: 'class',
+};
