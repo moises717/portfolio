c4f3ec9f3fc1ca4812c0a4f315ca98551b4a42dd
Moises Barillas <moisesrocha717@gmail.com>
2024-02-06 20:52:26 -0600

    Añadir archivos y configuraciones para soporte de React y Tailwind CSS

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,35 @@
+export function IconHome(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg height='21' viewBox='0 0 21 21' width='21' {...props}>
+			<g fill='none' fillRule='evenodd' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' transform='translate(1 1)'>
+				<path d='m.5 9.5 9-9 9 9' />
+				<path d='m2.5 7.5v7c0 1.1045695.8954305 2 2 2h10c1.1045695 0 2-.8954305 2-2v-7' />
+			</g>
+		</svg>
+	);
+}
+export function IconUser(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg height='21' viewBox='0 0 21 21' {...props}>
+			<path
+				d='m7.5.5c1.65685425 0 3 1.34314575 3 3v2c0 1.65685425-1.34314575 3-3 3s-3-1.34314575-3-3v-2c0-1.65685425 1.34314575-3 3-3zm7 14v-.7281753c0-3.1864098-3.6862915-5.2718247-7-5.2718247s-7 2.0854149-7 5.2718247v.7281753c0 .5522847.44771525 1 1 1h12c.5522847 0 1-.4477153 1-1z'
+				fill='none'
+				stroke='currentColor'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				transform='translate(3 2)'
+			/>
+		</svg>
+	);
+}
+export function IconMessage(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg height='21' viewBox='0 0 21 21' {...props}>
+			<g fill='none' fillRule='evenodd' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' transform='translate(2 3)'>
+				<path d='m11.4182262 1.21376122c-.904128-.29938651-1.88845542-.46376122-2.9182262-.46376122-4.418278 0-8 3.02593755-8 6.75862069 0 1.45741942.54603279 2.80709561 1.47469581 3.91098161l-.97469581 4.5803977 3.91607376-2.4472652c1.07810761.4571647 2.29544433.7145066 3.58392624.7145066 4.418278 0 8-3.0259376 8-6.75862071 0-.68476204-.1205394-1.34573924-.3446699-1.96861327' />
+				<path d='m14.5.5v4' />
+				<path d='m14.5.5v4' transform='matrix(0 1 -1 0 17 -12)' />
+			</g>
+		</svg>
+	);
+}
