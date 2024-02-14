65f56d6f5ab47b4fdebfb3013126f7fda8599721
Moises Barillas <moisesrocha717@gmail.com>
2024-02-13 18:28:17 -0600

    Actualizar enlaces de redes sociales y estilos oscuros

--- src/components/floating-navbar/index.tsx
+++ src/components/floating-navbar/index.tsx
@@ -37,7 +37,7 @@ export const FloatingNav = ({ className }: { className?: string }) => {
 					duration: 0.2,
 				}}
 				className={cn(
-					'flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4',
+					'flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent  rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1) dark:bg-gray-950  bg-gray-200 items-center justify-center space-x-4 sm:space-x-8 sm:pr-8 sm:pl-12 sm:py-4 sm:top-12 sm:inset-x-12 sm:rounded-3xl sm:0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] sm:z-[5000]  0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000]  pr-2 pl-8 py-2',
 					className,
 				)}
 			>
