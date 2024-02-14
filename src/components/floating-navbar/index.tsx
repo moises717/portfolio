25e164711d68e09a7073388e4e37e55344874273
Moises Barillas <moisesrocha717@gmail.com>
2024-02-13 19:31:07 -0600

    Agrega ID a la sección y enlaces de navegación actualizados

--- src/components/floating-navbar/index.tsx
+++ src/components/floating-navbar/index.tsx
@@ -42,7 +42,7 @@ export const FloatingNav = ({ className }: { className?: string }) => {
 				)}
 			>
 				<a
-					href={'/'}
+					href={'#home'}
 					className={cn(
 						'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500',
 					)}
@@ -64,7 +64,7 @@ export const FloatingNav = ({ className }: { className?: string }) => {
 					<span className='hidden sm:block text-sm'>Proyectos</span>
 				</a>
 				<a
-					href={'/'}
+					href={'#skills'}
 					className={cn(
 						'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500',
 					)}
