931a1ddf79f989e0b5bfb380aa7413666d586791
Moises Barillas <moisesrocha717@gmail.com>
2024-02-14 19:21:58 -0600

    Actualizar enlace de inicio en la barra de navegación flotante

--- src/components/floating-navbar/index.tsx
+++ src/components/floating-navbar/index.tsx
@@ -53,7 +53,7 @@ export const FloatingNav = ({ className }: { className?: string }) => {
 					<span className='hidden sm:block text-sm'>Inicio</span>
 				</a>
 				<a
-					href={'/'}
+					href={'#projects'}
 					className={cn(
 						'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500',
 					)}
