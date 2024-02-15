9e18de2774a496f4ee97e2e632affea7fbfb940d
Moises Barillas <moisesrocha717@gmail.com>
2024-02-15 17:38:45 -0600

    Agregado componente MouseShadow para seguir la posición del mouse

---
+++ src/components/mouse-shadow/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export function MouseShadow() {
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+
+	useEffect(() => {
+		const handleMove = (event: MouseEvent) => {
+			const { clientX, clientY } = event;
+			setPosition({ x: clientX, y: clientY });
+		};
+
+		window.addEventListener('pointermove', handleMove);
+
+		return () => {
+			window.removeEventListener('pointermove', handleMove);
+		};
+	});
+	return (
+		<div
+			className='pointer-events-none fixed inset-0 z-1 transition duration-300'
+			style={{
+				background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(29, 78, 216, 0.15), transparent 60%)`,
+			}}
+		/>
+	);
+}
