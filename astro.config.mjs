799f8d78ae408c3332b9bcc3604b4929ef262d46
Moises Barillas <moisesrocha717@gmail.com>
2024-02-17 18:06:26 -0600

    Agrega integración de sitemap a astro.config.mjs y package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	site: 'https://moisesbarillas.com',
+	integrations: [tailwind(), react(), sitemap()],
 });
